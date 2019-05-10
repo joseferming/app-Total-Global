91b275206c567e21275a4b096c4b50bddc3e92a8
joseferming <joseferming>
2019-05-10 15:13:48 -0430

    version 2

---
+++ catalogo/src/app/componentes/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../servicios/auth.service";
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+  onSubmitlogin()
+  {
+    console.log('funciona')
+  }
+}
---
+++ catalogo/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+export const firebaseconfig =  {
+  apiKey: "AIzaSyCGIhrFMN8SfpP2vxRuIXOKwHnCul56nbE",
+  authDomain: "total-global.firebaseapp.com",
+  databaseURL: "https://total-global.firebaseio.com",
+  projectId: "total-global",
+  storageBucket: "total-global.appspot.com",
+  messagingSenderId: "766292002789",
+  appId: "1:766292002789:web:7932f5db20f8863e"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
